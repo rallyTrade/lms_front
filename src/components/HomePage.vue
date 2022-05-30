37c4dff15be01e1edd6145c404f382aab97c78a5
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2022-05-30 09:46:57 +0100

    update on home and naviagtion

---
+++ src/components/HomePage.vue
@@ -0,0 +1,37 @@
+<template>
+    <Navigation />
+    <div class="container-fluid topBanner w-100">
+        <div class="container">
+            <div class="col-4 topBannerText d-md-flex align-items-center">
+                <h3 class="text-white">Welcome To LMS</h3>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Navigation from '../components/Navigation.vue'
+// import Navigation from 'Navigation.vue'
+
+export default {
+    components: {
+        Navigation
+    }
+}
+</script>
+<style>
+    .topBanner{ 
+        height: auto;
+    }
+
+    @media (min-width: 768px) {
+        .topBanner{
+            background: #1a1a1a;
+            background-image: url("../assets/topbanner.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-blend-mode: overlay;
+            height: 500px;
+        }
+    }
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <menu class="bg-dark container-fluid m-0">
+    <nav class="navbar navbar-expand-sm navbar-light bg-dark container">
+        <a class="navbar-brand" href="#">
+            <img src="../assets/logo.svg" alt="logo" />
+        </a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            
+        </div>
+    </nav>
+    </menu>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .navbar-brand img{ 
+        width: 120px;
+    }
+    @media (min-width: 768px) {
+        .navbar-brand img{ 
+        width: 150px;
+    }
+    }
+</style>
